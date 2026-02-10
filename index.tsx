c101903bd25b547d248f6b73fec7781eba7fdbd8
Pedro <yotucontents@gmail.com>
2026-02-10 18:13:06 +0100

    Prueba

--- index.tsx
+++ index.tsx
@@ -1,12 +1,10 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
-  throw new Error("No se pudo encontrar el elemento raíz");
+  throw new Error("Could not find root element to mount to");
 }
 
 const root = ReactDOM.createRoot(rootElement);
